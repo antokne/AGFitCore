c05e0c7eedeebc42cdd3cff59addce15d037f22d
Ants Gardiner <antokne@gmail.com>
2023-02-16 10:35:16 +1300

    Initial commit.

--- Sources/AGFitCore/AGFitCore.swift
+++ Sources/AGFitCore/AGFitCore.swift
@@ -1,6 +1,20 @@
 public struct AGFitCore {
-    public private(set) var text = "Hello, World!"
 
     public init() {
     }
+	
+
+	public static func calculateSampleRateforFitFile(with size: Int) -> Int {
+		var sampleRate = 0 // default include all samples
+		if size > 1000000 {
+			sampleRate = 30
+		}
+		else if size > 500000 {
+			sampleRate = 10
+		}
+		else if size > 100000 {
+			sampleRate = 5
+		}
+		return sampleRate
+	}
 }
