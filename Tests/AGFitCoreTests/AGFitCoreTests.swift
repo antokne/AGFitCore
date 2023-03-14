1a9c498666226d4143875234211bfd2da88878d8
Ants Gardiner <antokne@gmail.com>
2023-03-15 11:44:58 +1300

    Fixed Test.

--- Tests/AGFitCoreTests/AGFitCoreTests.swift
+++ Tests/AGFitCoreTests/AGFitCoreTests.swift
@@ -6,6 +6,5 @@ final class AGFitCoreTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AGFitCore().text, "Hello, World!")
     }
 }
