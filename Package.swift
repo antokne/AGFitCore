e8444d96d1f2f78206da1e6e5a8c8d6de3180351
Ants Gardiner <antokne@gmail.com>
2023-03-15 11:39:30 +1300

    changed to master for fitdataprotocol

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 		.package(url: "https://github.com/antokne/AGCore", branch: "develop"),
-		.package(url: "https://github.com/antokne/FitDataProtocol", branch: "feature/event-message-add-auto-generated-fields"),
+		.package(url: "https://github.com/antokne/FitDataProtocol", branch: "master"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
