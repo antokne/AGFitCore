769bc0362638548de08401972aa8ac1b6e793b89
Ants Gardiner <antokne@gmail.com>
2023-06-01 16:13:02 +1200

    Building out converter with tests.

---
+++ Sources/AGFitCore/Accumulator/AGAcummulatorConverter.swift
@@ -0,0 +1,376 @@
+//
+//  AGAcummulatorConverter.swift
+//  
+//
+//  Created by Antony Gardiner on 31/05/23.
+//
+
+import Foundation
+import AGCore
+import FitDataProtocol
+import AntMessageProtocol
+
+public enum AGConverterError: Error {
+	case failedToSaveFit
+}
+
+public struct AGDeveloperDataField {
+	var name: String
+	var fieldDefinitionNumber: UInt8
+	var baseUnit: BaseType
+	
+	/// Optional native message num that this field is attached to
+	var nativeMessageNum: UInt16?
+}
+
+public struct AGDeveloperData {
+	var developerDataIndex: UInt8 = 0 // a sensible default
+	var fields: [AGDeveloperDataField] = []
+}
+
+public struct AGConverterConfig {
+	public var name: String?
+	public var sport: Sport
+	public var subSport: SubSport
+	public var developerData: AGDeveloperData?
+}
+
+/// Converts raw data into fit messages.
+
+public class AGAcummulatorConverter {
+
+	private let acummulator: AGAccumulator
+	private let fitWriter: AGFitWriter
+	private let config: AGConverterConfig
+	
+	init(config: AGConverterConfig, acummulator: AGAccumulator, fitWriter: AGFitWriter) {
+		self.acummulator = acummulator
+		self.fitWriter = fitWriter
+		self.config = config
+	}
+	
+	// MARK: - convert raw messages into fit messages.
+	public func convert() async -> AGConverterError? {
+	
+		let startDate = acummulator.startDate ?? Date()
+		
+		// Add a file Id message.
+		fitWriter.appendMessage(message: createFileIdMessage(name: config.name, date: startDate))
+		
+		// Add developer fields if they exist.
+		if let devData = config.developerData {
+			
+			// Add developer data id messages
+			fitWriter.appendMessage(message: createDeveloperDataIdMessage())
+			
+			// Add Field description messages
+			for devField in devData.fields {
+				fitWriter.appendMessage(message: createFieldDescriptionMessage(name: devField.name,
+																			   developerDataIndex: devData.developerDataIndex,
+																			   fieldDefinitionNumber: devField.fieldDefinitionNumber,
+																			   messageNumber: devField.nativeMessageNum,
+																			   baseUnit: devField.baseUnit))
+			}
+		}
+
+		// Add sport messsage
+		fitWriter.appendMessage(message: createSportMessage(name: config.name,
+															sport: config.sport,
+															subSport: config.subSport))
+
+		// If we have sensor data we could also add device info messages.
+		
+		// Add a Start Event.
+		fitWriter.appendMessage(message: createEventMessage(date: startDate, eventType: .start))
+		
+		// Loop through all instand data and create record messages
+		let lastRecordDate = createRecordMessages(startDate: startDate)
+		
+		// Add a Stop all Event.
+		fitWriter.appendMessage(message: createEventMessage(date: lastRecordDate, eventType: .stopAll))
+		
+		// Add lap message at the end
+		fitWriter.appendMessage(message: createLapMessage(date: lastRecordDate, lapData: acummulator.lapData.currentData))
+
+		// Add session message at the end
+		fitWriter.appendMessage(message: createSessionMessage(date: lastRecordDate, sessionData: acummulator.sessionData.currentData))
+
+		// activity message at the very end
+		fitWriter.appendMessage(message: createActivityMessage(startTime: startDate, date: lastRecordDate, allSessions: acummulator.sessionData))
+		
+		return nil
+	}
+	
+	internal func createRecordMessages(startDate: Date) -> Date {
+	
+		var paused = false
+		
+		var lastRecordDate = startDate
+		
+		for second in acummulator.rawData.data.keys.sorted() {
+			
+			let recordDate = startDate.addingTimeInterval(TimeInterval(second))
+			
+			guard let instantData: AGAccumulatorRawInstantData = acummulator.rawData.data[second] else {
+				continue
+			}
+			
+			// if not paused and now paused then
+			if !paused && instantData.paused {
+				// add a pause event
+				fitWriter.appendMessage(message: createEventMessage(date: recordDate, eventType: .stop))
+			}
+			// if paused and now not paused, then
+			else if paused && !instantData.paused {
+				// add a resume event
+				fitWriter.appendMessage(message: createEventMessage(date: recordDate, eventType: .start))
+			}
+			
+			paused = instantData.paused
+			
+			if !paused {
+				// Add record messages
+				fitWriter.appendMessage(message: createRecordMessage(date: recordDate, rawData: instantData))
+			}
+			
+				
+			lastRecordDate = recordDate
+		}
+		return lastRecordDate
+	}
+	
+	// MARK: - Create messages
+
+	internal func createFileIdMessage(name: String? = nil,
+									  date: Date = Date(),
+									  manufacturerId: Int = 0,
+									  fileType: FileType = .activity) -> FileIdMessage {
+		
+		// time created -> Now
+		// Type 		-> Activity
+		// manufacturer -> Default to unknown.
+		
+		let fitTime = FitTime(date: date)
+		let manufacturer = Manufacturer.supportedManufacturers.first(where: { $0.manufacturerID == manufacturerId }) ?? Manufacturer.unknown
+		return FileIdMessage(fileCreationDate: fitTime,
+							 manufacturer: manufacturer,
+							 fileType: fileType,
+							 productName: name)
+	}
+	
+	internal func createFieldDescriptionMessage(name: String,
+												developerDataIndex: UInt8,
+												fieldDefinitionNumber: UInt8,
+												messageNumber: UInt16? = nil,
+												units: String? = nil,
+												baseUnit: BaseType) -> FieldDescriptionMessage {
+		
+		let baseType = BaseTypeData(type: baseUnit)
+		return FieldDescriptionMessage(dataIndex: developerDataIndex,
+									   definitionNumber: fieldDefinitionNumber,
+									   fieldName: name,
+									   baseInfo: baseType,
+									   units: units,
+									   baseUnits: nil,
+									   messageNumber: messageNumber,
+									   fieldNumber: nil) // do not use.
+	}
+
+	internal func createDeveloperDataIdMessage(devDataIndex: UInt8 = 0) -> DeveloperDataIdMessage {
+		return DeveloperDataIdMessage(dataIndex: devDataIndex)
+	}
+
+	internal func createSportMessage(name: String? = nil,
+									 sport: Sport,
+									 subSport: SubSport) -> SportMessage {
+		return SportMessage(name: name, sport: sport, subSport: subSport)
+	}
+	
+	internal func createRecordMessage(date: Date, rawData: AGAccumulatorRawInstantData) -> RecordMessage {
+		let fitTime = FitTime(date: date)
+		
+		// Position lat/lng
+		var position: Position? = nil
+		if let lat = rawData.value(for: .latitude),
+		   let lng = rawData.value(for: .longitude) {
+			let latMeasurement = Measurement(value: lat, unit: UnitAngle.degrees)
+			let lngMeasurement = Measurement(value: lng, unit: UnitAngle.degrees)
+			position = Position(latitude: latMeasurement, longitude: lngMeasurement)
+		}
+		
+		// Speed
+		var speedMeasurement: Measurement<UnitSpeed>? = nil
+		if let speed = rawData.value(for: .speed) {
+			speedMeasurement = Measurement(value: speed, unit: .metersPerSecond)
+		}
+		
+		// distance accum
+		var distanceMeasurement: Measurement<UnitLength>? = nil
+		if let distance = rawData.value(for: .distance) {
+			distanceMeasurement = Measurement(value: distance, unit: .meters)
+		}
+		
+		// altitude
+		var altitudeMeasurement: Measurement<UnitLength>? = nil
+		if let altitude = rawData.value(for: .altitude) {
+			altitudeMeasurement = Measurement(value: altitude, unit: .meters)
+		}
+		
+		// gps accuracy
+		var gpsAccuracyMeasurement: Measurement<UnitLength>? = nil
+		if let gpsVertAccuracy = rawData.value(for: .verticalAccuracy) {
+			gpsAccuracyMeasurement = Measurement(value: gpsVertAccuracy, unit: .meters)
+		}
+				
+		return RecordMessage(timeStamp: fitTime,
+							 position: position,
+							 distance: distanceMeasurement,
+							 altitude: altitudeMeasurement,
+							 speed: speedMeasurement,
+							 gpsAccuracy: gpsAccuracyMeasurement)
+	}
+	
+	internal func createEventMessage(date: Date,
+									 event: Event = .timer,
+									 eventType: EventType) -> EventMessage {
+		let fitTime = FitTime(date: date)
+		return EventMessage(timeStamp: fitTime, event: event, eventType: eventType)
+	}
+	
+	internal func createLapMessage(date: Date, lapData: AGAccumulatorData) -> LapMessage {
+		let fitTime = FitTime(date: date)
+		
+		// start time. of lap
+		let startTime = FitTime(date: lapData.startDate)
+		
+		// event type = stop
+		let eventType = EventType.stop
+		
+		// event = lap
+		let event = Event.lap
+		
+		// total lap time (paused + elapsed)
+		let totalTimeS = lapData.totalTimeS
+		let totalTimerTime: Measurement<UnitDuration> = Measurement(value: totalTimeS, unit: .seconds)
+		
+		// total elaspsed time
+		let totalElapsedTime: Measurement<UnitDuration> = Measurement(value: lapData.durationS, unit: .seconds)
+		
+		// total distance
+		var totalDistanceMeasurement: Measurement<UnitLength>? = nil
+		if let totalDistance = lapData.value(for: .distance, avgType: .last) {
+			totalDistanceMeasurement = Measurement(value: totalDistance, unit: .meters)
+		}
+
+		// avg speed
+		var averageSpeedMeasurement: Measurement<UnitSpeed>? = nil
+		if let avgSpeed = lapData.value(for: .speed, avgType: .average) {
+			averageSpeedMeasurement = Measurement(value: avgSpeed, unit: .metersPerSecond)
+		}
+		
+		return LapMessage(timeStamp: fitTime,
+						  event: event,
+						  eventType: eventType,
+						  startTime: startTime,
+						  totalElapsedTime: totalElapsedTime,
+						  totalTimerTime: totalTimerTime,
+						  totalDistance: totalDistanceMeasurement,
+						  averageSpeed: averageSpeedMeasurement)
+	}
+
+	internal func createSessionMessage(date: Date, sessionData: AGAccumulatorData) -> SessionMessage {
+		let fitTime = FitTime(date: date)
+		
+		// start time
+		let startTime = FitTime(date: sessionData.startDate)
+		
+		//event type = stop
+		let eventType = EventType.stop
+
+		// sport
+		let sport = config.sport
+		
+		// subsport
+		let subSport = config.subSport
+		
+		// event = session
+		let event = Event.session
+
+		// total session time (paused + elapsed)
+		let totalTimeS = sessionData.totalTimeS
+		let totalSessionTime: Measurement<UnitDuration> = Measurement(value: totalTimeS, unit: .seconds)
+		
+		// total elaspsed time
+		let totalElapsedTime: Measurement<UnitDuration> = Measurement(value: sessionData.durationS, unit: .seconds)
+		
+		// avg speed
+		var averageSpeedMeasurement: Measurement<UnitSpeed>? = nil
+		if let avgSpeed = sessionData.value(for: .speed, avgType: .average) {
+			averageSpeedMeasurement = Measurement(value: avgSpeed, unit: .metersPerSecond)
+		}
+		
+		// total distance
+		var totalDistanceMeasurement: Measurement<UnitLength>? = nil
+		if let totalDistance = sessionData.value(for: .distance, avgType: .last) {
+			totalDistanceMeasurement = Measurement(value: totalDistance, unit: .meters)
+		}
+		
+		// num laps
+		let numLaps: UInt16 = 1
+		
+		return SessionMessage(timeStamp: fitTime,
+							  event: event,
+							  eventType: eventType,
+							  startTime: startTime,
+							  sport: sport,
+							  subSport: subSport,
+							  totalElapsedTime: totalElapsedTime,
+							  totalTimerTime: totalSessionTime,
+							  totalDistance: totalDistanceMeasurement,
+							  averageSpeed: averageSpeedMeasurement,
+							  numberOfLaps: numLaps)
+	}
+	
+	internal func createActivityMessage(startTime: Date, date: Date, allSessions: AGAccumulatorMultiData) -> ActivityMessage {
+		let fitTime = FitTime(date: date)
+		
+		// local time stamp
+		let fitLocalTime = FitTime(date: startTime, isLocal: true)
+		
+		// event type = stop
+		let eventType = EventType.stop
+		
+		// activity type = manual
+		let activity = Activity.manual
+		
+		// event = activity
+		let event = Event.activity
+		
+		// total timer time (all sessions)
+		// num sessions
+		let numOfSessions: UInt16 = 1
+		
+		// this should be accumlative of all sessions.
+		let totalTimeS = allSessions.activityTotalTime
+		let totalSessionTime: Measurement<UnitDuration> = Measurement(value: totalTimeS, unit: .seconds)
+		
+		return ActivityMessage(timeStamp: fitTime,
+							   totalTimerTime: totalSessionTime,
+							   localTimeStamp: fitLocalTime,
+							   numberOfSessions: numOfSessions,
+							   activity: activity,
+							   event: event,
+							   eventType: eventType)
+	}
+	
+	public func write() async -> AGConverterError? {
+		
+		let result = fitWriter.write()
+		if result != nil {
+			return AGConverterError.failedToSaveFit
+		}
+		
+		return nil
+	}
+	
+}
---
+++ Tests/AGFitCoreTests/Accumulator/AGAccumulatorConverterTests.swift
@@ -0,0 +1,239 @@
+//
+//  AGAccumulatorConverterTests.swift
+//  
+//
+//  Created by Antony Gardiner on 1/06/23.
+//
+
+import XCTest
+@testable import AGFitCore
+import AGCore
+import FitDataProtocol
+import AntMessageProtocol
+
+final class AGAccumulatorConverterTests: XCTestCase {
+	
+	override func setUpWithError() throws {
+	}
+	
+	override func tearDownWithError() throws {
+	}
+	
+	func testFitMessageGenerationNoData() async throws {
+		
+		let url = try XCTUnwrap(URL.tempFitFile())
+		let fitWriter = MockAGFitWriter(fileURL: url)
+		
+		let config = AGConverterConfig(name: "Road Cycling", sport: .cycling, subSport: .road)
+		let accumulator = AGAccumulator()
+		
+		let date = Date()
+		accumulator.event(event: .start, at: date)
+		accumulator.event(event: .stop, at: date)
+		
+		let converter = AGAcummulatorConverter(config: config,
+											   acummulator: accumulator,
+											   fitWriter: fitWriter)
+		
+	
+		let result = await converter.convert()
+		XCTAssertNil(result)
+		
+		XCTAssertEqual(fitWriter.messages.count, 7)
+		
+		// File Id message
+		let fileIdMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? FileIdMessage) != nil }) as? FileIdMessage)
+		var timeInterval = try XCTUnwrap(fileIdMessage.fileCreationDate?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		
+		XCTAssertEqual("Road Cycling", fileIdMessage.productName)
+		XCTAssertEqual(FileType.activity, fileIdMessage.fileType)
+		XCTAssertEqual(Manufacturer.unknown, fileIdMessage.manufacturer)
+
+		// Sport Message
+		let sportMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? SportMessage) != nil }) as? SportMessage)
+		XCTAssertEqual(sportMessage.sport, Sport.cycling)
+		XCTAssertEqual(sportMessage.subSport, SubSport.road)
+
+		// Event messages
+		let eventMessages = try XCTUnwrap(fitWriter.messages.filter { ($0 as? EventMessage) != nil } as? [EventMessage])
+		XCTAssertEqual(eventMessages.count, 2)
+
+		// Event Message start
+		var eventMessage = try XCTUnwrap(eventMessages.first)
+		timeInterval = try XCTUnwrap(eventMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(eventMessage.eventType, EventType.start)
+
+		// Event Message stopall
+		eventMessage = try XCTUnwrap(eventMessages.last)
+		timeInterval = try XCTUnwrap(eventMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(eventMessage.eventType, EventType.stopAll)
+		
+		// Lap message
+		let lapMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? LapMessage) != nil }) as? LapMessage)
+		timeInterval = try XCTUnwrap(lapMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(lapMessage.totalTimerTime?.value, 0)
+		XCTAssertEqual(lapMessage.totalElapsedTime?.value, 0)
+		XCTAssertEqual(lapMessage.eventType, EventType.stop)
+		
+
+		// session message
+		let sessionMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? SessionMessage) != nil }) as? SessionMessage)
+		timeInterval = try XCTUnwrap(sessionMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(sessionMessage.totalTimerTime?.value, 0)
+		XCTAssertEqual(sessionMessage.totalElapsedTime?.value, 0)
+		XCTAssertEqual(sessionMessage.sport, Sport.cycling)
+		XCTAssertEqual(sessionMessage.subSport, SubSport.road)
+		XCTAssertEqual(sessionMessage.eventType, EventType.stop)
+
+		// activity message
+		let activityMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? ActivityMessage) != nil }) as? ActivityMessage)
+		timeInterval = try XCTUnwrap(activityMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(activityMessage.event, Event.activity)
+		XCTAssertEqual(activityMessage.numberOfSessions, 1)
+		XCTAssertEqual(activityMessage.activity, Activity.manual)
+		XCTAssertEqual(activityMessage.eventType, EventType.stop)
+	}
+	
+	func testFitMessageGenerationSomeData() async throws {
+		
+		let url = try XCTUnwrap(URL.tempFitFile())
+		let fitWriter = MockAGFitWriter(fileURL: url)
+		
+		let config = AGConverterConfig(name: "Road Cycling", sport: .cycling, subSport: .road)
+		let accumulator = AGAccumulator()
+		
+		let date = Date()
+		accumulator.event(event: .start, at: date)
+		
+		try accumulator.accumulate(date: date.plus(1), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(2), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(3), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(4), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(5), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(1), value: 1, type: .distance)
+		try accumulator.accumulate(date: date.plus(2), value: 2, type: .distance)
+		try accumulator.accumulate(date: date.plus(3), value: 3, type: .distance)
+		try accumulator.accumulate(date: date.plus(4), value: 4, type: .distance)
+		try accumulator.accumulate(date: date.plus(5), value: 5, type: .distance)
+
+		accumulator.event(event: .pause, at: date.plus(5))
+		
+		try accumulator.accumulate(date: date.plus(6), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(7), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(8), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(9), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(10), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(6), value: 5, type: .distance)
+		try accumulator.accumulate(date: date.plus(7), value: 5, type: .distance)
+		try accumulator.accumulate(date: date.plus(8), value: 5, type: .distance)
+		try accumulator.accumulate(date: date.plus(9), value: 5, type: .distance)
+		try accumulator.accumulate(date: date.plus(10), value: 5, type: .distance)
+		accumulator.event(event: .resume, at: date.plus(10))
+
+		try accumulator.accumulate(date: date.plus(11), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(12), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(13), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(14), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(15), value: 1, type: .speed)
+		try accumulator.accumulate(date: date.plus(11), value: 6, type: .distance)
+		try accumulator.accumulate(date: date.plus(12), value: 7, type: .distance)
+		try accumulator.accumulate(date: date.plus(13), value: 8, type: .distance)
+		try accumulator.accumulate(date: date.plus(14), value: 9, type: .distance)
+		try accumulator.accumulate(date: date.plus(15), value: 10, type: .distance)
+		
+		accumulator.event(event: .stop, at: date)
+		
+		let converter = AGAcummulatorConverter(config: config,
+											   acummulator: accumulator,
+											   fitWriter: fitWriter)
+		
+		
+		let result = await converter.convert()
+		XCTAssertNil(result)
+		
+		XCTAssertEqual(fitWriter.messages.count, 19)
+		
+		// File Id message
+		let fileIdMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? FileIdMessage) != nil }) as? FileIdMessage)
+		var timeInterval = try XCTUnwrap(fileIdMessage.fileCreationDate?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		
+		XCTAssertEqual("Road Cycling", fileIdMessage.productName)
+		XCTAssertEqual(FileType.activity, fileIdMessage.fileType)
+		XCTAssertEqual(Manufacturer.unknown, fileIdMessage.manufacturer)
+		
+		// Sport Message
+		let sportMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? SportMessage) != nil }) as? SportMessage)
+		XCTAssertEqual(sportMessage.sport, Sport.cycling)
+		XCTAssertEqual(sportMessage.subSport, SubSport.road)
+		
+		// Event messages
+		let eventMessages = try XCTUnwrap(fitWriter.messages.filter { ($0 as? EventMessage) != nil } as? [EventMessage])
+		XCTAssertEqual(eventMessages.count, 4)
+		
+		// Event Message start
+		var eventMessage = try XCTUnwrap(eventMessages.first)
+		timeInterval = try XCTUnwrap(eventMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(date.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(eventMessage.eventType, EventType.start)
+		
+		// record messages
+		let recordMessages = try XCTUnwrap(fitWriter.messages.filter { ($0 as? RecordMessage) != nil } as? [RecordMessage])
+		XCTAssertEqual(recordMessages.count, 10)
+		
+		let lastRecordMessage = try XCTUnwrap(recordMessages.last)
+		
+		let lastRecordMessageDate = try XCTUnwrap(lastRecordMessage.timeStamp?.recordDate)
+
+		// Event Message stopall
+		eventMessage = try XCTUnwrap(eventMessages.last)
+		timeInterval = try XCTUnwrap(eventMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(lastRecordMessageDate.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(eventMessage.eventType, EventType.stopAll)
+		
+		// Lap message
+		let lapMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? LapMessage) != nil }) as? LapMessage)
+		timeInterval = try XCTUnwrap(lapMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(lastRecordMessageDate.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(lapMessage.totalTimerTime?.value, 15)
+		XCTAssertEqual(lapMessage.totalElapsedTime?.value, 10)
+		XCTAssertEqual(lapMessage.totalDistance?.value, 10)
+		XCTAssertEqual(lapMessage.eventType, EventType.stop)
+		
+		
+		// session message
+		let sessionMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? SessionMessage) != nil }) as? SessionMessage)
+		timeInterval = try XCTUnwrap(sessionMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(lastRecordMessageDate.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(sessionMessage.totalTimerTime?.value, 15)
+		XCTAssertEqual(sessionMessage.totalElapsedTime?.value, 10)
+		XCTAssertEqual(sessionMessage.totalDistance?.value, 10)
+		XCTAssertEqual(sessionMessage.sport, Sport.cycling)
+		XCTAssertEqual(sessionMessage.subSport, SubSport.road)
+		XCTAssertEqual(sessionMessage.eventType, EventType.stop)
+		
+		// activity message
+		let activityMessage = try XCTUnwrap(fitWriter.messages.first(where: { ($0 as? ActivityMessage) != nil }) as? ActivityMessage)
+		timeInterval = try XCTUnwrap(activityMessage.timeStamp?.recordDate?.timeIntervalSinceReferenceDate)
+		XCTAssertEqual(lastRecordMessageDate.timeIntervalSinceReferenceDate, timeInterval, accuracy: 1)
+		XCTAssertEqual(activityMessage.event, Event.activity)
+		XCTAssertEqual(activityMessage.numberOfSessions, 1)
+		XCTAssertEqual(activityMessage.activity, Activity.manual)
+		XCTAssertEqual(activityMessage.eventType, EventType.stop)
+		XCTAssertEqual(activityMessage.totalTimerTime?.value, 15)
+	}
+}
+
+
+class MockAGFitWriter: AGFitWriter {
+	
+	override func write() -> AGFitWriterError? {
+		return nil
+	}
+}
